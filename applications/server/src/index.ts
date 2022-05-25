1774c8c4580cbb5ac8358778030c94b16d41f1d8
Etienne Brouillard <etiennebrouillard@outlook.com>
2022-05-25 19:20:16 -0400

    code improvments and stabilization support

--- applications/server/src/index.ts
+++ applications/server/src/index.ts
@@ -25,15 +25,13 @@ wss.on("connection", (inputWebSocket: WebSocket, request: any, client: any) => {
     const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
     const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
     const awsSessionToken = process.env.AWS_SESSION_TOKEN;
-    const languageCode = "en-US";
-    const region = process.env.AWS_DEFAULT_REGION;
-    const sampleRate = "44100";
+    const languageCode = path.searchParams.get("language") || "en-US";
+    const region = path.searchParams.get("region") || process.env.AWS_DEFAULT_REGION;
+    const sampleRate = path.searchParams.get("sampleRate") || "44100";
     const options = { awsAccessKeyId, awsSecretAccessKey, awsSessionToken, inputWebSocket, languageCode, path, region, sampleRate, transcribeClient } as TranscribeStreamingJobServiceOptions;
-
     inputWebSocket.on("close", (ev: Event) => {
         trace(`connection closed for ${path}`);
     });
-
     trace(`connection opened for path ${path}`);
     const transcribeStreamingJobService = new TranscribeStreamingJobService(options);
     transcribeStreamingJobService.transcribeStream();
--- applications/server/src/services/transcribe-streaming-job-service.ts
+++ applications/server/src/services/transcribe-streaming-job-service.ts
@@ -2,7 +2,7 @@ import { TranscribeStreamingClient } from "@aws-sdk/client-transcribe-streaming"
 import { createHash } from "crypto";
 import Debug from "debug";
 import { Readable } from "stream";
-import { Data, MessageEvent, WebSocket } from "ws";
+import { MessageEvent, WebSocket } from "ws";
 import * as marshaller from "@aws-sdk/eventstream-marshaller";
 import * as utilUtf8Node from "@aws-sdk/util-utf8-node";
 import * as v4 from "./crypto-service";
@@ -32,18 +32,13 @@ export class TranscribeStreamingJobService {
 
     private options: TranscribeStreamingJobServiceOptions;
     private transcribeSocket: WebSocket | undefined;
-    // private client: TranscribeStreamingClient;
 
     constructor(options: TranscribeStreamingJobServiceOptions) {
         this.options = options;
-        // this.client = this.options?.transcribeClient || new TranscribeStreamingClient({
-        //     region: this.options.region || "us-east-1"
-        // });
     }
 
     createPresignedURL() {
         const endpoint = "transcribestreaming." + this.options?.region + ".amazonaws.com:8443";
-
         // get a preauthenticated URL that we can use to establish our WebSocket
         return v4.createPresignedURL(
             "GET",
@@ -57,25 +52,9 @@ export class TranscribeStreamingJobService {
                 protocol: "wss",
                 expires: 15,
                 region: this.options?.region,
-                query: "language-code=" + this.options?.languageCode + "&media-encoding=pcm&sample-rate=" + this.options?.sampleRate
+                query: "partial-results-stability=medium&enable-partial-results-stabilization=true" + "&language-code=" + this.options?.languageCode + "&media-encoding=pcm&sample-rate=" + this.options?.sampleRate
             }
         );
-    }
-
-    getAudioEventMessage(buffer: Buffer): marshaller.Message {
-        return {
-            headers: {
-                ":message-type": {
-                    type: "string",
-                    value: "event"
-                },
-                ":event-type": {
-                    type: "string",
-                    value: "AudioEvent"
-                }
-            },
-            body: buffer
-        };
     }
 
     /**
@@ -94,21 +73,16 @@ export class TranscribeStreamingJobService {
             };
         };
         this.transcribeSocket.onmessage = (ev: MessageEvent) => {
-            // eslint-disable-next-line n/no-deprecated-api
-            const data = new Buffer(ev.data as any);
+            const data = Buffer.from(ev.data as any);
             const messageWrapper = eventStreamMarshaller.unmarshall(data);
             const body = Array.from(messageWrapper.body);
             const messageBody = JSON.parse(String.fromCharCode.apply(String, body));
             if (messageWrapper.headers[":message-type"].value === "event") {
                 trace("received event from Amazon Transcribe");
                 trace(JSON.stringify(messageBody));
-                // handleEventStreamMessage(messageBody);
             } else {
                 trace("received error from Amazon Transcribe");
                 trace(JSON.stringify(messageBody));
-                // transcribeException = true;
-                // showError(messageBody.Message);
-                // toggleStartStop();
             }
         };
     }
