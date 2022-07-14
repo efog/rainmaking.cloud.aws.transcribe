1dcc9282bb53a4fb2071ebb8f70baf83da53481d
Étienne Brouillard <etienne.brouillard@locomotive10.com>
2022-07-14 13:20:30 -0400

    move upload on audio socket close

--- applications/server/src/services/s3-service.ts
+++ applications/server/src/services/s3-service.ts
@@ -50,36 +50,38 @@ export class S3Service {
     }
     async uploadFromAudioSocket(inputAudioSocket: WebSocket): Promise<void> {
         if (inputAudioSocket) {
-            const crntDate = DateTime.utc();
-            const key = `audio/${crntDate.year}/${crntDate.month}/${crntDate.day}/${crntDate.hour}/${crntDate.hour}/${crntDate.minute}-${crntDate.second}-${this.callId}-${this.speakerName}.wav`;
-            const uploadMultiPartCommand = new CreateMultipartUploadCommand({
-                Bucket: this.targetBucketName,
-                Key: key,
-                StorageClass: "ONEZONE_IA"
-            });
-            const uploadMultiPartCommandOutput = await this.s3Client.send(uploadMultiPartCommand);
-            trace(`starting upload of ${JSON.stringify(uploadMultiPartCommandOutput)}`);
-            let partNumber = 0;
-            if (uploadMultiPartCommandOutput && uploadMultiPartCommandOutput.UploadId) {
-                const parts: CompletedPart[] = [];
-                inputAudioSocket.onmessage = async (evt: MessageEvent) => {
-                    partNumber++;
-                    const binData = evt.data as Buffer;
-                    trace(`uploading audio ${binData.length}`);
-                    const uploadPartCommand = new UploadPartCommand({
-                        Bucket: uploadMultiPartCommandOutput.Bucket,
-                        PartNumber: partNumber,
-                        Key: uploadMultiPartCommandOutput.Key,
-                        UploadId: uploadMultiPartCommandOutput.UploadId,
-                        Body: binData
-                    });
-                    const uploadPartCommandOutput = await this.s3Client.send(uploadPartCommand);
-                    parts.push({
-                        ETag: uploadPartCommandOutput.ETag,
-                        PartNumber: partNumber
-                    });
-                };
-                inputAudioSocket.onclose = async (evt: CloseEvent) => {
+            const binData: Buffer[] = [];
+            inputAudioSocket.onmessage = async (evt: MessageEvent) => {
+                binData.push(evt.data as Buffer);
+            };
+            inputAudioSocket.onclose = async (evt: CloseEvent) => {
+                const crntDate = DateTime.utc();
+                const key = `audio/${crntDate.year}/${crntDate.month}/${crntDate.day}/${crntDate.hour}/${crntDate.hour}/${crntDate.minute}-${crntDate.second}-${this.callId}-${this.speakerName}.wav`;
+                const uploadMultiPartCommand = new CreateMultipartUploadCommand({
+                    Bucket: this.targetBucketName,
+                    Key: key,
+                    StorageClass: "ONEZONE_IA"
+                });
+                const uploadMultiPartCommandOutput = await this.s3Client.send(uploadMultiPartCommand);
+                let partNumber = 0;
+                if (uploadMultiPartCommandOutput && uploadMultiPartCommandOutput.UploadId) {
+                    const parts: CompletedPart[] = [];
+                    for (let index = 0; index < binData.length; index++) {
+                        partNumber++;
+                        const data = binData[index];
+                        const uploadPartCommand = new UploadPartCommand({
+                            Bucket: uploadMultiPartCommandOutput.Bucket,
+                            PartNumber: partNumber,
+                            Key: uploadMultiPartCommandOutput.Key,
+                            UploadId: uploadMultiPartCommandOutput.UploadId,
+                            Body: data
+                        });
+                        const uploadPartCommandOutput = await this.s3Client.send(uploadPartCommand);
+                        parts.push({
+                            ETag: uploadPartCommandOutput.ETag,
+                            PartNumber: partNumber
+                        });
+                    }
                     const sortedParts = parts.sort((partA: CompletedPart, partB: CompletedPart) => { return (partA.PartNumber || 0) >= (partB.PartNumber || 0) ? 1 : -1; });
                     trace(`completed parts ${JSON.stringify(sortedParts)}`);
                     const completeMultipartUploadCommand = new CompleteMultipartUploadCommand({
@@ -95,8 +97,54 @@ export class S3Service {
                     } catch (err: any) {
                         error(err);
                     }
-                };
-            }
+                }
+            };
+            // const crntDate = DateTime.utc();
+            // const key = `audio/${crntDate.year}/${crntDate.month}/${crntDate.day}/${crntDate.hour}/${crntDate.hour}/${crntDate.minute}-${crntDate.second}-${this.callId}-${this.speakerName}.wav`;
+            // const uploadMultiPartCommand = new CreateMultipartUploadCommand({
+            //     Bucket: this.targetBucketName,
+            //     Key: key,
+            //     StorageClass: "ONEZONE_IA"
+            // });
+            // const uploadMultiPartCommandOutput = await this.s3Client.send(uploadMultiPartCommand);
+            // trace(`starting upload of ${JSON.stringify(uploadMultiPartCommandOutput)}`);
+            // let partNumber = 0;
+            // if (uploadMultiPartCommandOutput && uploadMultiPartCommandOutput.UploadId) {
+            //     const parts: CompletedPart[] = [];
+            //     inputAudioSocket.onmessage = async (evt: MessageEvent) => {
+            //         partNumber++;
+            //         const binData = evt.data as Buffer;
+            //         trace(`uploading audio ${binData.length}`);
+            //         const uploadPartCommand = new UploadPartCommand({
+            //             Bucket: uploadMultiPartCommandOutput.Bucket,
+            //             PartNumber: partNumber,
+            //             Key: uploadMultiPartCommandOutput.Key,
+            //             UploadId: uploadMultiPartCommandOutput.UploadId,
+            //             Body: binData
+            //         });
+            //         const uploadPartCommandOutput = await this.s3Client.send(uploadPartCommand);
+            //         parts.push({
+            //             ETag: uploadPartCommandOutput.ETag,
+            //             PartNumber: partNumber
+            //         });
+            //     };
+            //     inputAudioSocket.onclose = async (evt: CloseEvent) => {
+            //         const sortedParts = parts.sort((partA: CompletedPart, partB: CompletedPart) => { return (partA.PartNumber || 0) >= (partB.PartNumber || 0) ? 1 : -1; });
+            //         trace(`completed parts ${JSON.stringify(sortedParts)}`);
+            //         const completeMultipartUploadCommand = new CompleteMultipartUploadCommand({
+            //             Bucket: uploadMultiPartCommandOutput.Bucket,
+            //             Key: uploadMultiPartCommandOutput.Key,
+            //             UploadId: uploadMultiPartCommandOutput.UploadId,
+            //             MultipartUpload: {
+            //                 Parts: sortedParts
+            //             }
+            //         });
+            //         try {
+            //             const completeMultipartUploadCommandOutput = await this.s3Client.send(completeMultipartUploadCommand);
+            //         } catch (err: any) {
+            //             error(err);
+            //         }
+            //     };
         }
     }
 }
